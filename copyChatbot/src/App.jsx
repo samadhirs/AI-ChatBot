bbd2fd35a0cc42c56ad91db71f5639c8bf21e490
Samadhi Rasanjalee <126862124+samadhirs@users.noreply.github.com>
2025-12-20 15:09:34 +0530

    Header.jsx created

--- copyChatbot/src/App.jsx
+++ copyChatbot/src/App.jsx
@@ -3,8 +3,9 @@ import React from "react";
 function App()
   {
     return (
-    <div>
-      <h1 class="text-3xl font-bold underline">Hello World</h1>
+    <div className="flex flex-col h-screen">
+      <Header />
+      
     </div>
     );
   }
